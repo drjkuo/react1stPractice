18c39eaee7719491957994cea2987d56f8337180
KUO <drjkuo@gmail.com>
2017-05-03 09:42:09 -0700

    Pull out components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Panel from './Panel';
+import UserAndPass from './UserAndPass';
+import Header from './Header';
 import './App.css';
+
+let user = "Kuo";
+let pass = "Password";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Header />
+        <Panel />
+        <UserAndPass />
+        <div className="test"> {user} {pass}</div>
       </div>
     );
   }
---
+++ my-app/src/Panel.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+export default class Panel extends React.Component {
+    render(){
+      return (
+        <div className="App-header">
+          <div className="panel panel-primary">
+            <div className="panel-heading">
+              Sign in
+            </div>
+          </div>
+        </div>
+      );
+    }
+}
